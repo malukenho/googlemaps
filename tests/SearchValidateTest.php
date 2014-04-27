65fca2cacc2fcd0df85fe65c199e27cc35569cff
André Santos <afsdab@gmail.com>
2014-04-27 05:45:59 -0300

    Test to Class \Application\Validate\Search
    - check if no exist 'address' key in $_POST
    - check if exist 'address' key in $_POST

---
+++ tests/SearchValidateTest.php
@@ -0,0 +1,29 @@
+<?php 
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+use Application\Validate\Search as ValidateSearch; 
+
+class SearchValidateTest extends PHPUnit
+{
+    protected $_validateSearch = null;
+    public function setUp()
+    {
+        $this->_validateSearch = new ValidateSearch;
+    }
+
+    public function testVerificaSeNaoExisteChaveAddressNoPostRecebido()
+    {
+        $this->assertFalse($this->_validateSearch->isAddressExist(), "Não contém o endereço");
+    }
+
+    public function testVerificaSeExisteChaveAddressNoPostRecebido()
+    {
+        $_POST['address'] = null;
+        $this->assertFalse($this->_validateSearch->isAddressExist(), "Não contém o endereço");
+    }
+
+    public function tearDown()
+    {
+
+    }
+}
